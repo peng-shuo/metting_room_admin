7cd61adaa756d0254cf1c8ae80f4645f1285ce93
彭硕 <1148648853@qq.com>
2024-07-12 10:09:20 +0800

    feat: 管理端初始化

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,80 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Link,
+  Outlet,
+} from 'react-router-dom';
+import { Index } from './pages/Index/Index';
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
+import { UserManage } from './pages/UserManage/UserManage';
+import { Login } from './pages/Login/Login';
+import { Menu } from './pages/Menu/Menu';
+import { ModifyMenu } from './pages/ModifyMenu/ModifyMenu';
+import { InfoModify } from './pages/InfoModify/InfoModify';
+import { PasswordModify } from './pages/PasswordModify/PasswordModify';
+import { MeetingRoomManage } from './pages/MeetingRoomManage/MeetingRoomManage';
+import { BookingManage } from './pages/BookingManage/BookingManage';
+import { Statistics } from './pages/Statistics/Statistics';
+const routes = [
+  {
+    path: '/',
+    element: <Index></Index>,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/',
+        element: <Menu></Menu>,
+        children: [
+          {
+            path: '/',
+            element: <MeetingRoomManage />,
+          },
+          {
+            path: 'user_manage',
+            element: <UserManage />,
+          },
+          {
+            path: 'meeting_room_manage',
+            element: <MeetingRoomManage />,
+          },
+          {
+            path: 'booking_manage',
+            element: <BookingManage />,
+          },
+          {
+            path: 'statistics',
+            element: <Statistics />,
+          },
+        ],
+      },
+      {
+        path: '/user',
+        element: <ModifyMenu></ModifyMenu>,
+        children: [
+          {
+            path: 'info_modify',
+            element: <InfoModify />,
+          },
+          {
+            path: 'password_modify',
+            element: <PasswordModify />,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+];
+
+export const router = createBrowserRouter(routes);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router} />);
